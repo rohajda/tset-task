5c3a00ed58e9119902305adaaaf14fad4621421a
Robert Ohajda <robert.ohajda@alectoit.com>
2020-11-13 07:03:46 +0100

    Initialize spring boot project

---
+++ src/main/java/com/ohajda/tsettask/TsetTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ohajda.tsettask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsetTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsetTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohajda/tsettask/TsetTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohajda.tsettask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsetTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
